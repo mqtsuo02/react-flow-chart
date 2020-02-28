e4c1bd8136119bcbbaff68f4b047f1b2a85b19b4
mqtsuo02 <obu.tokyo@gmail.com>
2020-02-28 16:55:43 +0900

    Builded & Ignored dist

---
+++ dist/src/components/PortsGroup/PortsGroup.default.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import { IConfig } from '../../';
+export interface IPortsGroupDefaultProps {
+    config: IConfig;
+    side: 'top' | 'bottom' | 'left' | 'right';
+}
+export declare const PortsGroupDefault: import("styled-components").StyledComponentClass<import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & IPortsGroupDefaultProps, any, import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & IPortsGroupDefaultProps>;
